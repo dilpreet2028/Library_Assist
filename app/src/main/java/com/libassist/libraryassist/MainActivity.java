9cd436bd33bea396f7af7d4cda10f4ee71ec9204
rajdeep <iamrajdeep1008@gmail.com>
2015-06-14 23:21:23 +0530

    buttons finalised

--- app/src/main/java/com/libassist/libraryassist/MainActivity.java
+++ app/src/main/java/com/libassist/libraryassist/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -12,6 +13,9 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
     Database data;
     long id;
+    public void issue(View view){
+        Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show();
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,4 +53,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
